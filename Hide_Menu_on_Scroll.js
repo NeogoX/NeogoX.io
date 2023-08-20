8e6e39b6b5325698d76b6a67baf2c5d7271bb0e7
sqb <774370992@qq.com>
2023-08-20 21:13:28 +0800

    Update Hide_Menu_on_Scroll.js

--- Hide_Menu_on_Scroll.js
+++ Hide_Menu_on_Scroll.js
@@ -1,11 +1,9 @@
 /* When the user scrolls down, hide the du. When the user scrolls up, show the du */
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("du").style.top = "1500px";
-  } else {
-    document.getElementById("du").style.top = "-250px";
+
+
+
+$(window).scroll(function(){
+  if($(document).scrollTop() > 200){
+    $('.noe h1').css({'display': 'none'});
   }
-  prevScrollpos = currentScrollPos;
-}
+});

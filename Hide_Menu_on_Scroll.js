b3e88657c80a40a5846917c4f4b4e1ede0abbbcf
sqb <774370992@qq.com>
2023-08-20 21:34:50 +0800

    1

--- Hide_Menu_on_Scroll.js
+++ Hide_Menu_on_Scroll.js
@@ -1,9 +1,32 @@
-/* When the user scrolls down, hide the du. When the user scrolls up, show the du */
+function inViewport( element ){
+  
+  // Get the elements position relative to the viewport
 
+  var bb = element.getBoundingClientRect();
+  
+  // Check if the element is outside the viewport
+  // Then invert the returned value because you want to know the opposite
 
+  return !(bb.top > innerHeight || bb.bottom < 0);
+  
+}
 
-$(window).scroll(function(){
-  if($(document).scrollTop() > 1200){
-    $('.noe h1').css({'display': 'none'});
+var myElement = document.querySelector( 'div' );
+
+// Listen for the scroll event
+
+document.addEventListener( 'scroll', event => {
+ 
+  // Check the viewport status
+
+  if( inViewport( myElement ) ){
+    
+    myElement.style.opacity = 1;
+    
+  } else {
+    
+    myElement.style.opacity = '';
+    
   }
-});
+  
+})
